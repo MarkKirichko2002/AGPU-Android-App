96e7844884d865ef6c088f86c0f1efaa5846b41f
MarkDestroyer <58703027+MarkKirichko2002@users.noreply.github.com>
2023-07-29 10:57:53 +0300

    update
    Added screen "AGPU news".

--- AGPU/app/src/main/java/com/example/agpu/MainActivity.kt
+++ AGPU/app/src/main/java/com/example/agpu/MainActivity.kt
@@ -1,10 +1,25 @@
 package com.example.agpu
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GoToApp()
+    }
+
+    fun GoToApp() {
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                // Создаем интент для перехода на другой экран
+                val intent = Intent(this@MainActivity, NewsActivity::class.java)
+                startActivity(intent)
+                finish() // закрываем текущую активность, чтобы пользователь не мог вернуться к ней
+            }
+        }, 2000)
     }
 }
